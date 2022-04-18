83315504ef4c5566c1a1147af4c50e3c0eaf1126
Aikidoka2000 <48751242+Aikidoka2000@users.noreply.github.com>
2022-04-18 19:18:24 +0500

    ClassGame

--- SB_game.py
+++ SB_game.py
@@ -1,2 +1,11 @@
 # Sea_battle game
 
+class Game(obj):
+
+    sells = ("1", "2", "3", "4", "5", "6")
+    ships = [1, 1, 1, 1, 2, 2, 3]
+    map_size = len(letters)
+
+    def __init__(self):
+
+        
