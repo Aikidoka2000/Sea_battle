fa43b222b32af0440824eb7f3500def2641e5555
Aikidoka2000 <48751242+Aikidoka2000@users.noreply.github.com>
2022-04-20 01:18:25 +0500

    add Battlefield

--- SB_game.py
+++ SB_game.py
@@ -26,7 +26,7 @@ class Dot:
     def __repr__(self):
         return f"Dot({self.x}, {self.y})"
 
-    class Ship:
+class Ship:
     def __init__(self, bow, l, o):
         self.bow = bow
         self.l = l
@@ -51,4 +51,30 @@ class Dot:
         return ship_dots
     
     def shooten(self, shot):
-        return shot in self.dots    
+        return shot in self.dots
+
+class Board:
+    def __init__(self, hid = False, size = 6):
+        self.size = size
+        self.hid = hid
+        
+        self.count = 0
+        
+        self.field = [ ["O"]*size for _ in range(size) ]
+        
+        self.busy = []
+        self.ships = []
+    
+    def __str__(self):
+        res = ""
+        res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+        for i, row in enumerate(self.field):
+            res += f"\n{i+1} | " + " | ".join(row) + " |"
+        
+        if self.hid:
+            res = res.replace("■", "O")
+        return res
+    
+    def out(self, d):
+        return not((0<= d.x < self.size) and (0<= d.y < self.size))
+
