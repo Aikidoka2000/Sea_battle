51e1ed692c50379c4d87c3a24008d0b1592798ee
Aikidoka2000 <48751242+Aikidoka2000@users.noreply.github.com>
2022-04-20 01:00:53 +0500

    add class Dot

--- part1.py
+++ part1.py
@@ -1,5 +1,3 @@
-# Sea_battle game
-
 class Dot:
     def __init__(self, x, y):
         self.x = x
@@ -11,4 +9,13 @@ class Dot:
     def __repr__(self):
         return f"Dot({self.x}, {self.y})"
 
-        
+
+a = Dot(1, 1)
+b = Dot(3, 2)
+c = Dot(1, 1)
+aa = [Dot(1, 1), Dot(2, 4), Dot(6, 2), Dot(5, 2), Dot(1, 4)]
+
+print(a in aa)
+print(a)
+print(a == c)
+print(a.x == c.x and a.y == c.y) 
