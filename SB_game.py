13489edc5942b951c10890a213e38a7e2e092a22
Aikidoka2000 <48751242+Aikidoka2000@users.noreply.github.com>
2022-04-20 01:39:41 +0500

    add AI and Player

--- SB_game.py
+++ SB_game.py
@@ -1,5 +1,7 @@
 # Sea_battle game
 
+from random import randint
+
 class BoardException(Exception):
     pass
 
@@ -148,4 +150,32 @@ class Player:
                 return repeat
             except BoardException as e:
                 print(e)
+
+class AI(Player):
+    def ask(self):
+        d = Dot(randint(0,5), randint(0, 5))
+        print(f"Ход компьютера: {d.x+1} {d.y+1}")
+        return d
+
+class User(Player):
+    def ask(self):
+        while True:
+            cords = input("Ваш ход: ").split()
+            
+            if len(cords) != 2:
+                print(" Введите 2 координаты! ")
+                continue
+            
+            x, y = cords
+            
+            if not(x.isdigit()) or not(y.isdigit()):
+                print(" Введите числа! ")
+                continue
+            
+            x, y = int(x), int(y)
+            
+            return Dot(x-1, y-1)
+
+
+
 
